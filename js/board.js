57a527d4f0c79eb0d30ff5e6a01a2b3e92698d01
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-10-24 22:54:58 +0900

    차트생성
    생성만함

--- js/board.js
+++ js/board.js
@@ -1,9 +1,35 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
-    const mapDiv = document.getElementById('map');
+
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+      ];
+    
+      const data = {
+        labels: labels,
+        datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+      };
+    
+      const config = {
+        type: 'line',
+        data: data,
+        options: {}
+      };
 
 
-
-    const map = new google.maps.Map(mapDiv, mapOptions);
-    return map;
+      
+      const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
 })
