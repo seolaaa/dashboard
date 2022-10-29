6cb8590bcda9616e2c99f17851a97564e530d59a
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-10-29 19:05:42 +0900

    차트3개

--- js/board.js
+++ js/board.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded',()=>{
       const data1 = {
         labels: labels1,
         datasets: [{
-          label: 'My First dataset',
+          label: '올해 매출액',
           backgroundColor: 'rgb(255, 99, 132)',
           borderColor: 'rgb(255, 99, 132)',
           data: [0, 10, 5, 2, 20, 30, 45],
@@ -25,6 +25,9 @@ document.addEventListener('DOMContentLoaded',()=>{
         data: data1,
         options: {}
       };
+
+
+
 
 
  
@@ -40,7 +43,7 @@ document.addEventListener('DOMContentLoaded',()=>{
       const data2 = {
         labels: labels2,
         datasets: [{
-          label: 'Color',
+          label: '선호도',
           backgroundColor: 'rgb(255, 204, 051)',
           borderColor: 'rgb(255, 99, 132)',
           data: [20, 10, 5, 32, 12, 20, 15],
@@ -55,6 +58,42 @@ document.addEventListener('DOMContentLoaded',()=>{
       };
 
 
+
+
+      const labels3 = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+      ];
+    
+      const data3 = {
+        labels: labels3,
+        datasets: [{
+          label: '2022',
+          backgroundColor: 'rgb(255, 204, 051)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [20, 10, 5, 32, 12, 20, 15],
+        }],
+        datasets: [{
+          label: '2023',
+          backgroundColor: 'rgb(255, 198, 198)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [10, 30, 15, 22, 8, 12, 5],
+        }]
+      };
+     
+    
+      const config3 = {
+        type: 'bar',
+        data: data3,
+        options: {}
+      };
+
+
+
       
       const myChart1 = new Chart(
         document.getElementById('myChart1'),
@@ -64,5 +103,10 @@ document.addEventListener('DOMContentLoaded',()=>{
       const myChart2 = new Chart(
         document.getElementById('myChart2'),
         config2
+      );
+
+      const myChart3 = new Chart(
+        document.getElementById('myChart3'),
+        config3
       );
 })
