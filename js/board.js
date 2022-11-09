4bd8d4f344caacf8b648e562d1c2f6121f24ca4e
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-11-09 22:47:49 +0900

    11/9마무리

--- js/board.js
+++ js/board.js
@@ -1,141 +1,70 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
 
-    const labels1 = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-      ];
-    
-      const data1 = {
-        labels: labels1,
-        datasets: [{
-          label: '올해 매출액',
-          backgroundColor: 'rgb(255, 99, 132)',
-          borderColor: 'rgb(255, 99, 132)',
-          data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-      };
-    
-      const config1 = {
-        type: 'line',
-        data: data1,
-        options: {
-          
-        }
-      };
 
+  /*도넛차트*/
+    const labels7 = [
+      '이동혁',
+      '이마크',
+      '이제노',
+      '나재민',
+      '황인준',
+    ];
 
-
-
-      const labels3 = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-      ];
-    
-      const data3 = {
-        labels: labels3,
-        datasets: [{
-          label: '2022',
-          backgroundColor: 'rgb(204, 102, 153)',
-          borderColor: 'rgb(255, 99, 132)',
-          data: [20, 10, 5, 32, 12, 20, 15],
-        },
-        
-        {
-          label: '2023',
-          backgroundColor: 'rgb(255, 198, 198)',
-          borderColor: 'rgb(255, 99, 132)',
-          data: [10, 30, 15, 22, 8, 12, 5],
-        }],
-      };
-    
-      const config3 = {
-        type: 'bar',
-        data: data3,
-        options: {
-          maxBarThickness: 30,
-        }
-      };
-
-
-
-
-
-      const labels4 = [
-        '이동혁',
-        '이마크',
-        '이제노',
-        '나재민',
-        '황인준',
-        '종천러',
-      ];
-    
-      const data4 = {
-        labels: labels4,
-        datasets: [{
-          label: '상품 판매 수',
-          backgroundColor: ["#ff3366", "#a5ea89","#89a5ea","#336633","#59227c","#333366"],
-          borderColor: 'rgb(255, 255, 255)',
-          data: [20, 10, 5, 32, 12, 20],
-        }],
-      };
-    
-      const config4 = {
-        type: 'pie',
-        data: data4,
-        options: {
-          maintainAspectRatio :false,
-        }
-      };
-
-
-
-
-      const labels5 = [
-        '이동혁',
-        '이마크',
-        '이제노',
-        '나재민',
-        '황인준',
-      ];
-    
-      const data5 = {
-        labels: labels5,
-        datasets: [{
-          label: '상품 판매 수',
-          backgroundColor: ["#ff3366", "#a5ea89","#89a5ea","#336633","#59227c","#333366"],
-          borderColor: 'rgb(255, 255, 255)',
-          data: [20, 10, 5, 32, 20],
-        
-        }],
-      };
-    
-      const config5 = {
-        type: 'bar',
-        data: data5,
-        options: {
-          indexAxis: 'y',
-          maxBarThickness: 30,
-          maintainAspectRatio :false
-          
-      }
+    const data7 = {
+      labels: labels7,
+      datasets: [{
+        label: '상품 판매 수',
+        backgroundColor: ["#ff3366", "#a5ea89","#89a5ea","#336633","#59227c","#333366"],
+        borderColor: 'rgb(255, 255, 255)',
+        data: [20, 10, 5, 32, 20],
+      
+      }],
     };
 
+    const config7 = {
+      type: 'doughnut',
+      data: data7,
+      options: {
+        maintainAspectRatio :false,
+        
+    }
+  };
+  /*도넛차트끝*/
 
 
+  /*파이차트*/
+  const labels4 = [
+    '이동혁',
+    '이마크',
+    '이제노',
+    '나재민',
+    '황인준',
+    '종천러',
+  ];
 
- 
+  const data4 = {
+    labels: labels4,
+    datasets: [{
+      label: '상품 판매 수',
+      backgroundColor: ["#ff3366", "#a5ea89","#89a5ea","#336633","#59227c","#333366"],
+      borderColor: 'rgb(255, 255, 255)',
+      data: [20, 10, 5, 32, 12, 20],
+    }],
+  };
+
+  const config4 = {
+    type: 'pie',
+    data: data4,
+    options: {
+      maintainAspectRatio :false,
+    }
+  };
+  /*파이차트*/
+
   
-
-  const labels7 = [
+  /*y축 바 차트*/
+  const labels5 = [
     '이동혁',
     '이마크',
     '이제노',
@@ -143,8 +72,8 @@ document.addEventListener('DOMContentLoaded',()=>{
     '황인준',
   ];
 
-  const data7 = {
-    labels: labels7,
+  const data5 = {
+    labels: labels5,
     datasets: [{
       label: '상품 판매 수',
       backgroundColor: ["#ff3366", "#a5ea89","#89a5ea","#336633","#59227c","#333366"],
@@ -154,14 +83,70 @@ document.addEventListener('DOMContentLoaded',()=>{
     }],
   };
 
-  const config7 = {
-    type: 'doughnut',
+  const config5 = {
+    type: 'bar',
     data: data5,
     options: {
-      maintainAspectRatio :false
-      
+      indexAxis: 'y',
+      maxBarThickness: 30,
+      maintainAspectRatio :false,
   }
 };
+/*y축 바 차트끝*/
+
+
+/*x축 바 차트*/
+  const labels3 = [
+    'January',
+    'February',
+     'March',
+     'April',
+     'May',
+     'June',
+    ];
+    
+  const data3 = {
+    labels: labels3,
+    datasets: [{
+      label: '2022',
+      backgroundColor: 'rgb(204, 102, 153)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [20, 10, 5, 32, 12, 20, 15],
+    },
+        
+    {
+      label: '2023',
+      backgroundColor: 'rgb(255, 198, 198)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [10, 30, 15, 22, 8, 12, 5],
+    }],
+  };
+    
+  const config3 = {
+    type: 'bar',
+    data: data3,
+    options: {
+    maxBarThickness: 30,
+    }
+  };
+  /*x축 바 차트 끝*/
+
+
+
+
+
+
+
+
+  
+
+
+
+
+ 
+  
+
+
 
 
 
