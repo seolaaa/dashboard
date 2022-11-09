0c46055ba8609b39cee0c5b11b1b0b68b5460a14
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-11-09 22:22:21 +0900

    차트정리

--- js/board.js
+++ js/board.js
@@ -31,38 +31,6 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 
 
-
- 
-    const labels2 = [
-        'yellow',
-        'red',
-        'blue',
-        'pink',
-        'purple',
-        'green',
-      ];
-    
-      const data2 = {
-        labels: labels2,
-        datasets: [{
-          label: '선호도',
-          backgroundColor: 'rgb(255, 204, 051)',
-          borderColor: 'rgb(255, 99, 132)',
-          data: [20, 10, 5, 32, 12, 20, 15],
-        }]
-      };
-    
-      const config2 = {
-        type: 'bar',
-        data: data2,
-        options: {
-          maxBarThickness: 30,
-        }
-      };
-
-
-
-
       const labels3 = [
         'January',
         'February',
@@ -96,6 +64,8 @@ document.addEventListener('DOMContentLoaded',()=>{
           maxBarThickness: 30,
         }
       };
+
+
 
 
 
@@ -162,67 +132,69 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 
 
-    const labels6 = [
-      'Cake',
-      'Croissant',
-      'Cookie',
-      'Pie',
-      'Croffle',
-    ];
+ 
   
-    const data6 = {
-      labels: labels6,
-      datasets: [{
-        label: '카페 디저트 선호도',
-        data: [20, 10, 15, 25, 15],
-      
-      }],
-    };
-  
-    const config6 = {
-      type: 'line',
-      data: data6,
-      options: {
-        indexAxis: 'x',
-        maxBarThickness: 30,
-        maintainAspectRatio :false,
-        BackgroundColor :'rgb(255, 147, 187)',
-        borderColor: 'rgb(255, 147, 187)',
-        fill: false,
-        pointRadius: 0,
-        
-    }
+
+  const labels7 = [
+    '이동혁',
+    '이마크',
+    '이제노',
+    '나재민',
+    '황인준',
+  ];
+
+  const data7 = {
+    labels: labels7,
+    datasets: [{
+      label: '상품 판매 수',
+      backgroundColor: ["#ff3366", "#a5ea89","#89a5ea","#336633","#59227c","#333366"],
+      borderColor: 'rgb(255, 255, 255)',
+      data: [20, 10, 5, 32, 20],
+    
+    }],
   };
 
+  const config7 = {
+    type: 'doughnut',
+    data: data5,
+    options: {
+      maintainAspectRatio :false
+      
+  }
+};
+
+
+
 
       
+    
       const myChart1 = new Chart(
-        document.getElementById('sales'),
-        config1
+        document.getElementById('doughnut'),
+        config7
       );
+      
 
       const myChart2 = new Chart(
-        document.getElementById('preference'),
-        config2
-      );
-
-      const myChart3 = new Chart(
-        document.getElementById('comparison'),
-        config3
-      );
-
-      const myChart4 = new Chart(
         document.getElementById('name'),
         config4
       );
 
-      const myChart5 = new Chart(
+
+      const myChart3 = new Chart(
         document.getElementById('bar'),
         config5
       );
 
-      const myChart6 = new Chart(
-        document.getElementById('dessert'),
-        config6
+
+      const myChart4 = new Chart(
+        document.getElementById('comparison'),
+        config3
       );
-})
+
+     
+     
+});
+
+     
+
+      
