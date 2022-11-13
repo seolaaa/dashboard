7653b7f22dbc64605ab739b9507926f395df6101
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-11-13 19:31:15 +0900

    맵추가

--- js/board.js
+++ js/board.js
@@ -157,6 +157,16 @@ document.addEventListener('DOMContentLoaded',()=>{
   );
   /*차트실행끝*/
 
+  /*지도시작*/
+  // var container = document.getElementById('map');
+	// 	var options = {
+	// 		center: new kakao.maps.LatLng(33.450701, 126.570667),
+	// 		level: 3
+	// 	};
+
+	// 	var map = new kakao.maps.Map(container, options);
+  /*지도끝*/
+
      
      
 });
